0016161c905acc8897152b7d21a0b9c4d6b61a1b
emylincon <emylincon@gmail.com>
2019-03-18 12:33:42 +0000

    updated the files to be dynamic

--- create_websites.py
+++ create_websites.py
@@ -3,6 +3,9 @@ import os
 
 
 for i in range(1,21):
-    os.system('echo "Hello world" > /usr/share/nginx/html/{}.txt'.format(i))
-    os.system('echo "This is web {}" >> /usr/share/nginx/html/{}.txt'.format(i, i))
-    os.system('echo "Test page {} for Caching setup" >> /usr/share/nginx/html/{}.txt'.format(i, i))
+    os.system('echo "Hello world" > /usr/share/nginx/html/{}.html'.format(i))
+    os.system('echo "This is web {}" >> /usr/share/nginx/html/{}.html'.format(i, i))
+    os.system('echo "Test page {} for Caching setup" >> /usr/share/nginx/html/{}.html'.format(i, i))
+
+os.system('rm /etc/nginx/conf.d/default.conf')
+os.system('mv default.conf /etc/nginx/conf.d/')
