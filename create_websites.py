a93ba8b7cd84cd5ffa4a9e3151aeb2195e5c4fcf
Emeka E Ugwuanyi <emylincon@gmail.com>
2019-03-18 12:57:53 +0000

    Update create_websites.py

--- create_websites.py
+++ create_websites.py
@@ -7,5 +7,5 @@ for i in range(1,21):
     os.system('echo "This is web {}" >> /usr/share/nginx/html/{}.html'.format(i, i))
     os.system('echo "Test page {} for Caching setup" >> /usr/share/nginx/html/{}.html'.format(i, i))
 
-os.system('rm /etc/nginx/conf.d/default.conf')
-os.system('mv default.conf /etc/nginx/conf.d/')
+#os.system('rm /etc/nginx/conf.d/default.conf')
+#os.system('mv default.conf /etc/nginx/conf.d/')
